6fad62dca056c8dfc1476d98983727de08fdd11c
Victor Mauricio Restrepo Ossa <victor.restrepoo@upb.edu.co>
2019-10-14 18:25:34 -0500

    Create account sucessfull

--- js/main.js
+++ js/main.js
@@ -2,3 +2,9 @@ function changeSection(sectionIn,sectionOut){
     document.getElementById(sectionIn).style.display = 'block';
     document.getElementById(sectionOut).style.display = 'none';
 }
+
+function createUserAccount(){
+
+    changeSection('containerElementsCASucefull','containerElementsCA');
+    return false;
+}
