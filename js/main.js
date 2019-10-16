0660cb8604b852496cef1b42b89dd1088c82eacb
Victor Mauricio Restrepo Ossa <victor.restrepoo@upb.edu.co>
2019-10-16 12:11:24 -0500

    Basic creation and login with firebase

--- js/main.js
+++ js/main.js
@@ -1,17 +1,48 @@
-function changeSection(sectionIn,sectionOut){
+function changeSection(sectionIn, sectionOut) {
     document.getElementById(sectionIn).style.display = 'block';
     document.getElementById(sectionOut).style.display = 'none';
 }
 
-function createUserAccount(){
+function createUserAccount() {
+    let emailUser = document.getElementById('emailCA').value;
+    let userName = document.getElementById('userNameCA').value;
+    let passwordUser = document.getElementById('passCA').value;
 
-    changeSection('containerElementsCASucefull','containerElementsCA');
+
+    auth.createUserWithEmailAndPassword(emailUser, passwordUser).then(function (data) {
+        changeSection('containerElementsCASucefull', 'containerElementsCA');
+        alert('Usuario creado');
+        auth.currentUser.sendEmailVerification().then(function () {
+            // Email sent.
+        }).catch(function (error) {
+            // An error happened.
+        });
+    })
+        .catch(function (error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            if (errorMessage === "The email address is already in use by another account.") {
+                alert("Ya existe una cuenta de usuario asociada a ese correo electrónico");
+            }
+        });
+
     return false;
 }
 
-
 function userLogin(){
+    let email = document.getElementById('emailLogin').value;
+    let password = document.getElementById('passLogin').value;
 
+    auth.signInWithEmailAndPassword(email, password).then(function (){
+        alert('Correcto');
 
-    return false;
+    }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+      });
+
+      return false;
 }
