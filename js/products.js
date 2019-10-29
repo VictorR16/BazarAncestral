a591b8aacde1738f6b12cabfc7bd999cbd423e9d
Juan Daniel Garcia Lopez <31321197+juandagalo@users.noreply.github.com>
2019-10-28 22:36:20 -0500

    Dinamic store showcase

---
+++ js/products.js
@@ -0,0 +1,27 @@
+window.onload = function () {
+    
+    const app = firebase.app();
+
+    let products_showcase = this.document.getElementById('tableProducts');
+    const db = firebase.firestore();
+    const productsRef = db.collection('products')
+
+    productsRef.get()
+            .then(products => {
+                products.forEach(doc => {
+                    data = doc.data()
+                    
+                    products_showcase.innerHTML += 
+                    '<tr id="products-info">'+
+                        '<td class="tdTable">'+
+                            '<div class="containerProducts">'+
+                                '<img class="imgProducts" src="'+data.image+'">'+
+                                '<h4 class="nameProducts">'+data.name+'</h4>'+
+                                '<p class="priceProducts">'+data.price+'</p>'+
+                            '</div>'+
+                        '</td>'+
+                    '</tr>';
+                });
+            })
+
+}
